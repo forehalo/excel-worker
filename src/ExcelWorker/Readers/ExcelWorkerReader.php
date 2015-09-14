4862f9e698fe91be26798c33dadb17a605645478
刘义 <forehalo@gmail.com>
2015-09-14 17:09:52 +0800

    ExcelWorkerReader

---
+++ src/ExcelWorker/Readers/ExcelWorkerReader.php
@@ -0,0 +1,164 @@
+<?php namespace ExcelWorker\Readers;
+
+use PHPExcel;
+use ExcelWorker\Exception\ExcelWorkerException;
+
+/**
+ * Class ExcelWorkerReader.php
+ * @package     forehalo/excel-worker
+ * @version     1.0.0
+ * @copyright   Copyright (c) 2015 forehalo <http://www.forehalo.top>
+ * @author      forehalo <forehalo@gmail.com>
+ * @license     http://www.gnu.org/licenses/lgpl.html   LGPL
+ */
+class ExcelWorkerReader
+{
+    /**
+     * All cells of the file except the header(first row if specified).
+     *
+     * @var array $content
+     */
+    protected $content = [];
+
+    /**
+     * file name
+     *
+     * @var string
+     */
+    protected $file = '';
+
+    /**
+     * The extension of file.
+     *
+     * @var string $extension
+     */
+    protected $extension = '';
+
+    /**
+     * Header (Always or Default the first row if specified) of file/sheet.
+     *
+     * @var array $header
+     */
+    protected $header = [];
+
+    /**
+     * Constructor
+     * Load file into $content and parse the first row into $header.
+     *
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get all the content as a index array.
+     *
+     * @return array
+     * @throws ExcelWorkerException     When nothing loaded.
+     */
+    public function get(PHPExcel $excel)
+    {
+        $sheetNum = 0;
+        foreach ($excel->getWorksheetIterator() as $worksheets) {
+            array_push($this->content, [$sheetNum => []]);
+            $rowNum = 0;
+            foreach ($worksheets->getRowIterator() as $row) {
+                array_push($this->content[$sheetNum], [$rowNum => []]);
+                foreach ($row->getCellIterator() as $cell) {
+                    if(!is_null($cell))
+                        array_push($this->content[$sheetNum][$rowNum], $cell->getValue());
+                }
+                $rowNum++;
+            }
+            $sheetNum++;
+        }
+        return $this->content;
+    }
+
+    /**
+     * Get one row by the given row number.
+     *
+     * @param int $row number of row
+     * @return array
+     * @throws ExcelWorkerException     When a number given less than 1 or greater than count of row.
+     */
+    public function getRow($row)
+    {
+    }
+
+    /**
+     * Get the first row. Perhaps the second row if header exist.
+     *
+     * @throws ExcelWorkerException
+     */
+    public function getFirst()
+    {
+    }
+
+    /**
+     * Get one column by given column number except header.
+     *
+     * @param int $col number of column
+     * @return array
+     * @throws ExcelWorkerException     When a number given less than 1 or greater than count of column.
+     */
+    public function getColumn($col)
+    {
+    }
+
+    /**
+     * Get cell content.
+     *
+     * @param $row number of row
+     * @param $col number of column
+     * @return string cell content
+     * @throws ExcelWorkerException     When index of cell given is invalid.
+     */
+    public function getCell($row, $col)
+    {
+    }
+
+    /**
+     * Get the header if specified.
+     *
+     * @return array
+     */
+    public function getHeader()
+    {
+    }
+
+    /**
+     * Judge whether the given row exist in $content.
+     *
+     * @param int $row number of row
+     * @return bool
+     */
+    public function rowExist($row)
+    {
+    }
+
+    /**
+     * Judge Whether the given column exist in $content.
+     *
+     * @param int $col number of column
+     * @return bool
+     */
+    public function columnExist($col)
+    {
+    }
+
+    /**
+     * Judge Whether the Given index of a cell exist in $content.
+     *
+     * @param int $row number of row
+     * @param int $col number of column
+     * @return bool
+     */
+    public function cellExist($row, $col)
+    {
+    }
+
+    public function canReader()
+    {
+    }
+}
